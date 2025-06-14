762d28a46c933a923fda068f52c2a420df96aafc
login9000 <pmax462@yahoo.com>
2025-06-14 14:30:03 +0300

    update

--- unix_socket/client.py
+++ unix_socket/client.py
@@ -3,43 +3,87 @@
 import re
 import socket
 import traceback
+import time
 
 
 result = b''
 err = ''
 echo_server_unix_socket = '/tmp/echo_server_unix_socket.sock'
 data = 'Hello world!'
+counter_request = 0
 
-try:
-	
-	sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-	sock.settimeout(5)
-	sock.connect(echo_server_unix_socket)
-	sock.sendall((data+'\n').encode('utf-8'))
-	
-	while True:
-		_data = sock.recv(1024)
-		if len(_data) == 0:
-			break
-		result += _data
-		try:
-			if re.search(b'(\r|\n|\r\n)$', _data):
-				break
-		except Exception:
-			pass
-	result = result.decode('utf-8')
+_t3 = time.time()
+_counter_request = 0
+_number_of_requests_per_second = 0
+_d_min = 10
+_d_max = 0
+y = 0
+_d_sum = 0
+is_first_second = False
 
-except Exception:
+for _ in range(10000):
 
-	print('err:', re.sub('\r?\n', '', traceback.format_exc().strip()))
-
-finally:
-	
 	try:
-		sock.close()
-	except:
-		pass
+			
+		_t1 = time.time()
 
-print('result:', result)
+		sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+		sock.settimeout(5)
+		sock.connect(echo_server_unix_socket)
+		sock.sendall((data+'\n').encode('utf-8'))
+		
+		while True:
+			_data = sock.recv(1024)
+			if len(_data) == 0:
+				break
+			result += _data
+			try:
+				if re.search(b'(\r|\n|\r\n)$', _data):
+					break
+			except Exception:
+				pass
+		result = result.decode('utf-8')
 
+		_t2 = time.time()
+
+		counter_request += 1
+		_counter_request = counter_request
+
+		y += 1
+
+		d = _t2 - _t1
+		_d_sum += d
+
+		if _d_min > d:
+			_d_min = d
+
+		if _d_max < d:
+			_d_max = d
+
+		if time.time() - _t3 >= 1: # 
+			is_first_second = True
+			_t3 = time.time()
+			print('min: '+str(_d_min)+', max: '+str(_d_max)+', average: '+str((_d_sum) / y)+', number of requests per second: '+str(_counter_request - _number_of_requests_per_second))
+			print('result:', result)
+			print('------------------------------------------')
+			_number_of_requests_per_second = _counter_request
+			y = 0
+			_d_sum = 0
+
+	except Exception:
+
+		print('err:', re.sub('\r?\n', '', traceback.format_exc().strip()))
+		break
+
+	finally:
+		
+		try:
+			sock.close()
+		except:
+			pass
+
+if not is_first_second:
+	print('min: '+str(_d_min)+', max: '+str(_d_max)+', average: '+str((_d_sum) / y)+', number of requests per second: '+str(_counter_request - _number_of_requests_per_second))
+	print('result:', result)
+	print('------------------------------------------')
 
